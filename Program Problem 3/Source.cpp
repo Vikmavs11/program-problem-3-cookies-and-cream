a881edcb0fa207812caf7234f3ec0ae439c3aa3a
Vikmavs11 <31861754+Vikmavs11@users.noreply.github.com>
2017-10-12 12:54:03 -0500

    Update Source.cpp

--- Program Problem 3/Source.cpp
+++ Program Problem 3/Source.cpp
@@ -11,7 +11,7 @@ Create a project assuming that a completely trusthrowrthy and perfect user comes
 //namespaces
 using namespace std; //*
 
-//functions()
+					 //functions()
 void pause() {
 	cout << "Press any key to continue...";
 	while (!_kbhit());
@@ -20,29 +20,37 @@ void pause() {
 }
 // main
 void main() {
-	int someThreeDigitNumber;
+	for (int i = 0; i < 30; i++) {
+		int someThreeDigitNumber;
 	cout << "Enter a three digit number : ";
 	cin >> someThreeDigitNumber;
 	int A, B, C;
-	
-	A = someThreeDigitNumber / 100 ;
-	
-	B = (someThreeDigitNumber / 10) % 10;
-	
-	C = someThreeDigitNumber % 10;
 
-	if (A<B && B<C) { // check to see if ascending
-		
-		cout << "The number is ascending";
+
+	
+	{
+
+		A = someThreeDigitNumber / 100;
+
+		B = (someThreeDigitNumber / 10) % 10; // make the names of the variables more accurate.
+
+		C = someThreeDigitNumber % 10;
+
+		if (A < B && B < C) { // check to see if ascending
+
+			cout << someThreeDigitNumber << " is ascending" << endl;
+		}
+		else if (A > B && B > C) { // check to see if descending
+
+			cout << someThreeDigitNumber << " is descending" << endl;
+		}
+		else { //check to see if neither 
+
+			cout << someThreeDigitNumber << " The number is neither ascending nor descending" << endl;
+		}
 	}
-	else if (A>B && B>C) { // check to see if descending
-		
-		cout << "The number is descending";
-	}
-	else if (A=B=C) { //check to see if neither
-		
-		cout << "The number is neither ascending nor descending" << endl;
-	}
+
+
 	pause(); // pauses to see the displayed text
-
+}
 }
